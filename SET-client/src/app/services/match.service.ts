9744bebbe61f4918b8952a0aeaf24d53cb478823
Daniel Madden <danielfpjobs@gmail.com>
2021-05-24 13:01:06 -0600

    small tweak I missed. hashed out logic a bit more

--- SET-client/src/app/services/match.service.ts
+++ SET-client/src/app/services/match.service.ts
@@ -10,13 +10,13 @@ export class MatchService {
 
   private cardProperties: string[] = ['count', 'color', 'shape', 'shade'];
 
-  checkMatches(cards: Card[]): CardMatchCheck {
+  checkMatches(cards: [Card, Card, Card]): CardMatchCheck {
     let matches!: CardMatchCheck;
 
     this.cardProperties.forEach((prop) => {
       let match = true;
       for (let i = 0; i < 2; i++) {
-        if (cards[i][prop] === cards[i + 1][prop]) match = !match;
+        if (cards[i][prop] !== cards[i + 1][prop]) match = !match;
       }
       matches[prop] = match;
     });
@@ -29,17 +29,22 @@ export class MatchService {
  * checkMatch logic
  *
  * All different:
- * Start with true. Second value is different, swap the value.
- * Now false. Third value is different, swap the value.
- * Now true.
+ * Start with true. Second value is different, swap boolean.
+ * Now false. Third value is different, swap boolean.
+ * End: True.
  *
  * All true:
- * Start with true. Second value is same, keep value.
- * Still true. Third value is same, keep value.
- * Still true.
+ * Start with true. Second value is same, keep boolean.
+ * Still true. Third value is same, keep boolean.
+ * End: True.
  *
  * Two same:
- * Start with true. Second value is same, keep value.
- * Still true. Third value is different, swap value.
- * Now false.
+ * Start with true. Second value is same, keep boolean.
+ * Still true. Third value is different, swap boolean.
+ * End: False.
+ *
+ * Two different:
+ * Start with true. Second value is different, swap boolean.
+ * Now false. Third value is same, keep boolean.
+ * End: False.
  */
