abcfcd133eeaa0859466b0969afdb0ba652fa4dd
Daniel Madden <danielfpjobs@gmail.com>
2021-05-25 15:36:34 -0600

    FINALLY testing my deck methods

--- SET-client/src/app/pages/page-practice/page-practice.component.ts
+++ SET-client/src/app/pages/page-practice/page-practice.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { Card } from 'src/app/models/card';
+import { DeckService } from 'src/app/services/deck.service';
 
 @Component({
   selector: 'app-page-practice',
   templateUrl: './page-practice.component.html',
-  styleUrls: ['./page-practice.component.scss']
+  styleUrls: ['./page-practice.component.scss'],
 })
 export class PagePracticeComponent implements OnInit {
+  constructor(private deckService: DeckService) {}
 
-  constructor() { }
+  deck!: Observable<Card[]>;
 
   ngOnInit(): void {
+    this.deck = this.deckService.deck$;
+    this.deck.subscribe(
+      (deck) => console.log(deck),
+      (err) => console.log(`[ERROR] ${err}`),
+      () => console.log(`Complete`)
+    );
+    this.deckService.createDeck();
+    this.deckService.shuffleDeck();
+    this.deckService.resetDeck();
   }
-
 }
