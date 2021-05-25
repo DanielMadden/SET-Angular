bb44c6f02f39d8ca630d67ac92148faf9b214054
Daniel Madden <danielfpjobs@gmail.com>
2021-05-25 15:29:48 -0600

    Base Routes

--- SET-client/src/app/shared/routes.ts
+++ SET-client/src/app/shared/routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from '../app.component';
+import { PagePracticeComponent } from '../pages/page-practice/page-practice.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'practice',
+    component: PagePracticeComponent,
+  },
+  {
+    path: '',
+    component: AppComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
