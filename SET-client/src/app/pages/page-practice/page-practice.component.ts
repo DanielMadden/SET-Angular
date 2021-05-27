b4290e7d513c1f7db23fe361ee4092a6de8e3a21
Daniel Madden <danielfpjobs@gmail.com>
2021-05-27 12:51:18 -0600

    If I ever need to test again.

--- SET-client/src/app/pages/page-practice/page-practice.component.ts
+++ SET-client/src/app/pages/page-practice/page-practice.component.ts
@@ -19,3 +19,39 @@ export class PagePracticeComponent implements OnInit {
 
   ngOnInit(): void {}
 }
+
+/**
+ *
+ this.deck = this.deckService.deck$;
+    this.deck.subscribe(
+      (deck) => console.log(deck),
+      (err) => console.log(`[ERROR] ${err}`),
+      () => console.log(`Complete`)
+    );
+    this.deckService.createDeck();
+    this.deckService.shuffleDeck();
+    this.deckService.resetDeck();
+
+    console.log(
+      this.matchService.checkIfCardsMatch([
+        {
+          color: 1,
+          count: 1,
+          shade: 'solid',
+          shape: 'diamond',
+        },
+        {
+          color: 1,
+          count: 1,
+          shade: 'hollow',
+          shape: 'diamond',
+        },
+        {
+          color: 1,
+          count: 1,
+          shade: 'striped',
+          shape: 'diamond',
+        },
+      ])
+    );
+ */
