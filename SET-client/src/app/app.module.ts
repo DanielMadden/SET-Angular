b7b8e764d02f55c8e184807aa5b26dc238e42091
Daniel Madden <danielfpjobs@gmail.com>
2021-05-27 15:09:11 -0600

    Angular Material

--- SET-client/src/app/app.module.ts
+++ SET-client/src/app/app.module.ts
@@ -4,8 +4,9 @@ import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { routes } from './shared/routes';
+import { materials } from './shared/materials';
+
 import { PagePracticeComponent } from './pages/page-practice/page-practice.component';
 import { BarTopComponent } from './components/bars/bar-top/bar-top.component';
 import { BarBottomMobileComponent } from './components/bars/bar-bottom-mobile/bar-bottom-mobile.component';
@@ -18,10 +19,31 @@ import { GameLogMessageCardComponent } from './components/game-log/game-log-mess
 import { SetCardDeckComponent } from './components/set-card/set-card-deck/set-card-deck.component';
 import { SetCardComponent } from './components/set-card/set-card/set-card.component';
 import { SetCardGridComponent } from './components/set-card/set-card-grid/set-card-grid.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent, PagePracticeComponent, BarTopComponent, BarBottomMobileComponent, BarBottomDesktopComponent, BarSideMobileComponent, BarSideMobileStatsComponent, GameLogComponent, GameLogMessageComponent, GameLogMessageCardComponent, SetCardDeckComponent, SetCardComponent, SetCardGridComponent],
-  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(routes)],
+  declarations: [
+    AppComponent,
+    PagePracticeComponent,
+    BarTopComponent,
+    BarBottomMobileComponent,
+    BarBottomDesktopComponent,
+    BarSideMobileComponent,
+    BarSideMobileStatsComponent,
+    GameLogComponent,
+    GameLogMessageComponent,
+    GameLogMessageCardComponent,
+    SetCardDeckComponent,
+    SetCardComponent,
+    SetCardGridComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
+    ...materials,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ SET-client/src/app/shared/materials.ts
@@ -0,0 +1,4 @@
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+export const materials = [MatButtonModule, MatIconModule];
