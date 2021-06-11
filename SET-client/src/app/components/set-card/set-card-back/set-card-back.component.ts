b40651142bfba826dc2f03cad63edde8829e85bd
Daniel Madden <danielfpjobs@gmail.com>
2021-06-11 10:47:59 -0600

    +3 Cards slides in on the set deck

--- SET-client/src/app/components/set-card/set-card-back/set-card-back.component.ts
+++ SET-client/src/app/components/set-card/set-card-back/set-card-back.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-set-card-back',
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class SetCardBackComponent {
   constructor() {}
+
+  @Input() slideForButtons: boolean = false;
 }
