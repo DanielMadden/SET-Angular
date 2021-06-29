fe4bffa76fc7ffbbfccedaee3b1a1bc8b897dac8
Daniel Madden <danielfpjobs@gmail.com>
2021-06-29 11:01:17 -0700

    Responsive Design: Mobile bottom bar

--- SET-client/src/app/components/bars/bar-bottom-mobile/bar-bottom-mobile.component.ts
+++ SET-client/src/app/components/bars/bar-bottom-mobile/bar-bottom-mobile.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { gameBarHeight } from 'src/app/shared/variables';
 
 @Component({
   selector: 'app-bar-bottom-mobile',
   templateUrl: './bar-bottom-mobile.component.html',
-  styleUrls: ['./bar-bottom-mobile.component.scss']
+  styleUrls: ['./bar-bottom-mobile.component.scss'],
 })
 export class BarBottomMobileComponent implements OnInit {
+  public barHeight = gameBarHeight;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
