6c7dad2d3878d317068ba9f0f8f2e440f11fcec6
Daniel Madden <danielfpjobs@gmail.com>
2021-07-08 13:58:59 -0700

    Mobile buttons set up for emit to parent

--- SET-client/src/app/components/bars/bar-bottom-mobile/bar-bottom-mobile.component.ts
+++ SET-client/src/app/components/bars/bar-bottom-mobile/bar-bottom-mobile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { gameBarHeight } from 'src/app/shared/variables';
 
 @Component({
@@ -9,7 +9,23 @@ import { gameBarHeight } from 'src/app/shared/variables';
 export class BarBottomMobileComponent implements OnInit {
   public barHeight = gameBarHeight;
 
+  @Output() openGameLogEvent = new EventEmitter();
+  @Output() goHomeEvent = new EventEmitter();
+  @Output() plusThreeCardsEvent = new EventEmitter();
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  emitButtonPress(event: 'gameLog' | 'goHome' | 'plusThreeCards') {
+    if (event === 'gameLog') {
+      this.openGameLogEvent.emit();
+    }
+    if (event === 'goHome') {
+      this.goHomeEvent.emit();
+    }
+    if (event === 'plusThreeCards') {
+      this.plusThreeCardsEvent.emit();
+    }
+  }
 }
