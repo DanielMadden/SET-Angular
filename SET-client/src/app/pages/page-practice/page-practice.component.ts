3ef179c6e95469a1aaf69bcc9b25acf9085f7181
Daniel Madden <danielfpjobs@gmail.com>
2021-07-12 12:57:13 -0600

    mobile game log stats permanent height. game log height reactively changes to fill out the rest of the column.

--- SET-client/src/app/pages/page-practice/page-practice.component.ts
+++ SET-client/src/app/pages/page-practice/page-practice.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { interval, Observable, Subject } from 'rxjs';
+import { fromEvent, interval, Observable, Subject } from 'rxjs';
 import { ICard } from 'src/app/models/card';
 import { GameLog, IGameLog } from 'src/app/models/game-log';
 import { DeckService } from 'src/app/services/deck.service';
@@ -38,6 +38,8 @@ export class PagePracticeComponent implements OnInit {
   public cardsRemaining = 81;
 
   public showGameLogOnMobile = false;
+  public gameLogOnMobileStatsHeight = 169;
+  public gameLogOnMobileHeight = window.innerHeight - this.gameLogOnMobileStatsHeight;
 
   ngOnInit(): void {
     this.deck$ = this.deckService.deck$;
@@ -46,6 +48,7 @@ export class PagePracticeComponent implements OnInit {
       this.deck = deck;
       this.cardsRemaining = this.deck.length;
     });
+    this.subscribeToPageResize()
   }
 
   public startGame() {
@@ -114,4 +117,10 @@ export class PagePracticeComponent implements OnInit {
   }
 
   public travelHome() {}
+
+  private subscribeToPageResize() {
+    fromEvent(window, 'resize').subscribe(() => {
+      this.gameLogOnMobileHeight = window.innerHeight - this.gameLogOnMobileStatsHeight;
+    })
+  }
 }
