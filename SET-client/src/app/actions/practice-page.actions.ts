8787da6217d7b119942907702bfcdf5172016ffe
Daniel Madden <danielfpjobs@gmail.com>
2021-07-26 14:23:58 -0600

    Practice page actions

---
+++ SET-client/src/app/actions/practice-page.actions.ts
@@ -0,0 +1,15 @@
+import { createAction, props } from '@ngrx/store';
+
+export const enter = createAction('[Practice Page] Enter');
+
+export const addThreeCards = createAction('[Practice Page] Add Three Cards');
+
+export const selectCard = createAction(
+  '[Practice Page] Select Card',
+  props<{ cardGridSlotIndex: number }>()
+);
+
+export const deselectCard = createAction(
+  '[Practice Page] Deselect Card',
+  props<{ cardGridSlotIndex: number }>()
+);
